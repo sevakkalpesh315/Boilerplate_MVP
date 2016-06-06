8fdceb68abe46dd4fbd0c6c532c532c767a38dea
kalpesh sevak <sevakkalpesh@gmail.com>
2016-06-06 17:07:26 +0100

    Dagger modules

---
+++ app/src/main/java/math/sevakkalpesh/com/boilerplate_mvp/di/modules/NetModule.java
@@ -0,0 +1,76 @@
+package math.sevakkalpesh.com.boilerplate_mvp.di.modules;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+
+@Module
+public class NetModule {
+
+    String mBaseUrl;
+
+    public NetModule(String baseUrl) {
+        this.mBaseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences providesSharedPreferences(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+    @Provides
+    @Singleton
+    Cache provideOkHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024; // 10 MiB
+        Cache cache = new Cache(application.getCacheDir(), cacheSize);
+        return cache;
+    }
+
+
+    @Provides  // Dagger will only look for methods annotated with @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setCache(cache);
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        okHttpClient.interceptors().add(interceptor);
+        return okHttpClient;
+    }
+
+    @Provides
+    @Singleton
+    RestAdapter provideRetrofit(OkHttpClient okhttp) {
+
+        RestAdapter retrofit = new RestAdapter.Builder()
+                //.addConverterFactory(GsonConverterFactory.create(gson))
+                .setEndpoint(mBaseUrl)
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .setClient(new OkClient(okhttp))
+                .build();
+        return retrofit;
+    }
+}
